243dbd2d3d8f9f091636761ce3627499ca4c284d
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-16 21:29:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,8 @@ def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
   elsif array.length == 3
-    array.insert(-2, "and")
     array.join(", ")
   else array.length >= 4
-    array.insert(-2, "and")
     array.join(", ")
   end
 end
