c4faf926286db8426a99d10c09645385605c4751
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-16 21:19:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
+  elsif array.length = 3
+    array.join(", ")
+  else array.length >= 4
+    array.join(", ")
   end
   
 end
